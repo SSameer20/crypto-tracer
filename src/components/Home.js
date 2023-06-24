f94205fd6d9035fb5c58d2ddf0fe5a1d5e059efd
Sameer Shaik <sameer.kattubadi@gmail.com>
2023-06-24 08:49:19 +0530

    home update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,44 @@
-import React,{useState,useEffect} from 'react'
-import  axios  from 'axios';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import './Home.css'
 
-export default function Home(){
-    const [search,setSearch] = useState('');
-    const [data,setData] = useState([]);
-    useEffect(()=>{
+export default function Home() {
+    const [search, setSearch] = useState('');
+    const [data, setData] = useState([]);
+    useEffect(() => {
         axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=100&currency=INR').then(
             res => {
                 console.log(res.data.coins);
                 setData(res.data.coins);
             });
-            
-    },[])
 
-    const filterData = data.filter((coin)=>{
+    }, [])
+
+    const filterData = data.filter((coin) => {
         return coin.name.toLowerCase().includes(search.toLowerCase());
     });
-    
-  
 
-    return(
+
+
+    return (
         <>
-        <div className="navigation">
-            <h1 className="heading">Online Crypto Price Tracker</h1>
-            <input value={search} onChange={(event) => {setSearch(event.target.value)}} className="search" placeholder=' Search for Crypto'/>
-        </div>
+            <div className="navigation">
+                <h1 className="heading">Online Crypto Price Tracker</h1>
+                <input value={search} onChange={(event) => { setSearch(event.target.value) }} className="search" placeholder=' Search for Crypto' />
+            </div>
 
-        <div className="container">
-            {filterData.map(crypto =>
-        <div className="crypto-card">
-            <img src={crypto.icon} className="crypto-img" alt="" />
-            <h2>{crypto.name}</h2>
-            <h4>Symbol : ' {crypto.symbol} '</h4>
-            <h4>price : ₹ {crypto.price}</h4>
-            <h4>1h Price Change : {crypto.priceChange1h}</h4>
-  
-        </div>
-            )}
+            <div className="container">
+                {filterData.map(crypto =>
+                    <div className="crypto-card">
+                        <img src={crypto.icon} className="crypto-img" alt="" />
+                        <h2>{crypto.name}</h2>
+                        <h4>Symbol : ' {crypto.symbol} '</h4>
+                        <h4>price : ₹ {crypto.price}</h4>
+                        <h4>1h Price Change : {crypto.priceChange1h}</h4>
+
+                    </div>
+                )}
+
 
             </div>
         </>
